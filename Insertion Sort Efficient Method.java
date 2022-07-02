64540c57e1e9f5a65adc860ab0bf2f07d02c8ff4
Arup Basak <65061893+arup2001-basak@users.noreply.github.com>
2022-07-03 01:11:03 +0530

    Update Insertion Sort Efficient Method.java

--- Insertion Sort Efficient Method.java
+++ Insertion Sort Efficient Method.java
@@ -26,13 +26,16 @@ output:
 arr[] = {5, 6, 11, 12, 13}
 */
 
-void insertionSort(int arr[], int n){
-  for(int i = 1; i < n ; i++){
-    int key = arr[i];
-    int j = i - 1;
-    while(j>=0 && arr[j]>key){
-      arr[j+1] = arr[j];
-      j--;
-    }
-    arr[j
-    
+void instertionSort(int arr[], int n){
+          for(int i = 1; i<n; i++){
+                    int key = arr[i];
+                     int j = i-1;
+                     while(j>=0 && arr[j]>key)
+                     {
+                               arr[j+1] = arr[j];
+                               j--;
+                     }
+                    arr[j+1] = key;
+          }
+}
+                               
